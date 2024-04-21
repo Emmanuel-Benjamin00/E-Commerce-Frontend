c9e9163e73d089d0fbf1c9127e9d9421313f83e1
Emmanuel Benjamin D <emmanuel26112000@gmail.com>
2024-04-21 23:50:30 +0530

    server link added

--- src/utils/Data.js
+++ src/utils/Data.js
@@ -2,5 +2,5 @@ export const commonFile =
   "https://res.cloudinary.com/dmq6qfdjs/image/upload/v1700501899/ovfxoidib4tqwga62osf.jpg";
 
 export const DEV_API_URL = "http://localhost:8000/";
-export const PROD_API_URL = "https://e-commerce-backend-7qjj.onrender.com/";
+export const PROD_API_URL = "https://e-commerce-backend-frkx.onrender.com/";
 
